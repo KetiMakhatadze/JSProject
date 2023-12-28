efad01fbe5aa8aee297396c38546625f37686136
Keti Makhatadze <keti.maxatadze@gau.edu.ge>
2023-12-28 11:08:57 +0400

    Second commit

--- node.js
+++ node.js
@@ -1,22 +1,3 @@
-// let currentIndex = 0;
-//         const slides = document.querySelectorAll('.slide');
-
-//         function showSlide(index) {
-//             slides.forEach((slide, i) => {
-//                 slide.style.transform = `translateX(${(i - index) * 100}%)`;
-//             });
-//         }
-
-//         function nextSlide() {
-//             currentIndex = (currentIndex + 1) % slides.length;
-//             showSlide(currentIndex);
-//         }
-
-//         function prevSlide() {
-//             currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-//             showSlide(currentIndex);
-//         }
-
 const sliders = [
     {
         id: 1,
